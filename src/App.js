cace862333f6353d395ad2828f57dbb13e68738d
Jorge Garay <jorge.garay.reuque@gmail.com>
2021-10-27 22:13:25 -0300

    navbar MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavBar from './components/navbar/navbar';
+import List from './components/list/list'
 
-function App() {
+function App() {  
   return (
     <div className="App">
       <NavBar/>
+      <List>
+        </List>             
     </div>
   );
 }
